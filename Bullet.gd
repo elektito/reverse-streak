1d43c1d01b3303fcb4cee3fb672e49d1d5082a86
Mostafa Razavi <mostafa@sepent.com>
2021-11-18 08:02:04 +0100

    Use a visibility notifier to despawn bullets

--- Bullet.gd
+++ Bullet.gd
@@ -5,10 +5,11 @@ export(float) var speed:= 600.0
 
 func _physics_process(delta):
 	position += direction * speed * delta
-	
-	if position.y < -$sprite.texture.get_size().y / 2:
-		queue_free()
 
 
 func _on_Bullet_area_entered(area):
+	queue_free()
+
+
+func _on_visibility_notifier_screen_exited():
 	queue_free()
